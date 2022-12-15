714fd03d870a226184405000f2ec6edd3a61c0c3
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2022-12-15 23:48:17 +0530

    feat: redux setup complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import { costomTheme } from './styles/theme';
+import { Provider } from 'react-redux'
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -15,7 +17,9 @@ root.render(
     <ChakraProvider theme={costomTheme}>
       <ColorModeScript />
       <BrowserRouter>
-      <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
