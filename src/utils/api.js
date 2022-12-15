0ed62d3824f6c75430a560b787ecffb497be9d64
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2022-12-15 23:55:09 +0530

    feat: api setup complete

---
+++ src/utils/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { variables } from '../constants/backend';
+
+export const _post = (url, payload) => {
+  return axios.post(`${variables.BACKEND_URL}${url}`, payload, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+};
+
+export const _get = (url) => {
+  return axios.get(`${variables.BACKEND_URL}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+};
+
+export const _put = (url, payload) => {
+  return axios.put(`${variables.BACKEND_URL}${url}`, payload, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+};
